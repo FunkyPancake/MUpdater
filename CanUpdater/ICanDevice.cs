be77285654bc9f239fc60fff14f20eb4814c54fc
FunkyPancake <matdeb01685@gmail.com>
2023-02-03 10:25:42 +0100

    update can interface

--- CanUpdater/ICanDevice.cs
+++ CanUpdater/ICanDevice.cs
@@ -1,14 +1,18 @@
 namespace CanUpdater;
 
+
+ 
 public interface ICanDevice
 {
+    public delegate void NewFrameHandler(CanFrame frame);
     public bool Connect();
     public void Configure(CanDeviceConfig config);
     public void Disconnect();
     public void SendFrame(CanFrame frame);
-    public void SendFrameCyclic(CanFrame frame, int cycleTime);
-    public void SubscribeFrame();
-    public void UnsubscribeFrame();
-    public void GetFrame();
+    public void StartSendFrameCyclic(CanFrame frame, int cycleTime);
+    public void StopSendFrameCyclic(CanFrame frame);
+    public void SubscribeFrame(CanFrame frame, NewFrameHandler handler);
+    public void UnsubscribeFrame(CanFrame frame);
+    public void GetFrame(ref CanFrame frame);
     public List<string> GetAvailableChannels();
 }
